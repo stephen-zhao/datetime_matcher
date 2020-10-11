e44a487b9bda5b9c5b570658ea2213ce447c80c9
Stephen Zhao <mail@zhaostephen.com>
2020-10-11 02:19:09 -0400

    Add setup to package-ify repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='datetime_matcher',
+    version='0.1.0',
+    author='Stephen Zhao',
+    author_email='mail@zhaostephen.com',
+    description='A library which extends regex with support for datetime format codes.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/stephen-zhao/datetime_matcher',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

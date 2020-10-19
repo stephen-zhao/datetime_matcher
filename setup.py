b4e0c64dc99215945552a6a30c0bbb35332de64d
Stephen Zhao <mail@zhaostephen.com>
2020-10-18 23:05:18 -0400

    Update version + add keywords and classifiers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='datetime_matcher',
-    version='0.1.4',
+    version='0.1.5',
     author='Stephen Zhao',
     author_email='mail@zhaostephen.com',
     description='A library which extends regex with support for datetime format codes.',
@@ -17,8 +17,21 @@ setuptools.setup(
         'datetime parsing',
         'datetime format',
         'find and replace',
+        'search and replace',
         'substitution',
         'regular expression',
+        'format',
+        'strftime',
+        'strptime',
+        'parse',
+        'reformat',
+        'date',
+        'time',
+        'format code',
+        'search',
+        'find',
+        'replace',
+        'match',
     ],
     url='https://github.com/stephen-zhao/datetime_matcher',
     packages=setuptools.find_packages('src'),
@@ -26,9 +39,15 @@ setuptools.setup(
         'datetime_matcher': 'src/datetime_matcher'
     },
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
     ],
     python_requires='>=3.6',
+    license='MIT License'
 )
